7fbbd90f5fb3c6f958e2469fa2f0e0b52bf7a2c6
Moonlight Shinesparkle <bryanvale1107@gmail.com>
2026-02-12 08:49:18 -0800

    Added initial splashscreen

---
+++ SplashScreen.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace GoodsFlow
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // SplashScreen
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 450);
+            ControlBox = false;
+            Cursor = Cursors.AppStarting;
+            FormBorderStyle = FormBorderStyle.None;
+            IsMdiContainer = true;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "SplashScreen";
+            Opacity = 0.9D;
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            SizeGripStyle = SizeGripStyle.Hide;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form2";
+            TopMost = true;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
