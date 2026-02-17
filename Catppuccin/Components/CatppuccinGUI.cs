e7521854d3e635da7ed473097c7ea8e543816b2a
Moonlight Shinesparkle <bryanvale1107@gmail.com>
2026-02-16 18:41:40 -0800

    turned dashboard into catppuccin enabled form

--- Catppuccin/Components/CatppuccinGUI.cs
+++ Catppuccin/Components/CatppuccinGUI.cs
@@ -7,6 +7,7 @@ public class CatppuccinButton : Button, ICatppuccinEnabled
 {
     public CatppuccinButton() : base()
     {
+        ApplyCatppuccin();
     }
 
     private CatppuccinColor BCC = CatppuccinColor.Crust;
@@ -24,6 +25,43 @@ public class CatppuccinButton : Button, ICatppuccinEnabled
     public CatppuccinColor ForegroundColor {
         get => FCC; set { 
             FCC = value; 
+            ApplyCatppuccin();
+        }
+    }
+
+    public void ApplyCatppuccin()
+    {
+        BackColor = CatppuccinPallette.GetColor(BackgroundColor);
+        ForeColor = CatppuccinPallette.GetColor(ForegroundColor);
+    }
+}
+
+public class CatppuccinForm : Form, ICatppuccinEnabled
+{
+    public CatppuccinForm() : base()
+    {
+        ApplyCatppuccin();
+    }
+
+    private CatppuccinColor BCC = CatppuccinColor.Crust;
+    private CatppuccinColor FCC = CatppuccinColor.Crust;
+    [Category("Catppuccin")]
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+    public CatppuccinColor BackgroundColor
+    {
+        get => BCC; set
+        {
+            BCC = value;
+            ApplyCatppuccin();
+        }
+    }
+    [Category("Catppuccin")]
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+    public CatppuccinColor ForegroundColor
+    {
+        get => FCC; set
+        {
+            FCC = value;
             ApplyCatppuccin();
         }
     }
---
+++ Windows/Dashboard.cs
@@ -0,0 +1,24 @@
+﻿using GoodsFlow.Catppuccin.Components;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GoodsFlow.Windows
+{
+    public partial class Dashboard : CatppuccinForm
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
