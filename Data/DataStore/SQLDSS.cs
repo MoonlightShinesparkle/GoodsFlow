f1caf6d11df96f56fc7706ee81608058a9da4e78
Moonlight Shinesparkle <bryanvale1107@gmail.com>
2026-02-23 20:24:38 -0800

    Placeholder future data access points

---
+++ Data/DataStore/SQLDSS.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoodsFlow.Data.DataStore;
+
+public class SQLDSS
+{
+    // TODO: SQL server connection and functions
+}
---
+++ Data/Local/FSS.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoodsFlow.Data.Local;
+
+public class FSS
+{
+    // TODO: add file storage service miscellanea
+}
